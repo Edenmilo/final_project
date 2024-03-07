4695af181e9dfc008fad8d3b7983d0355d8a6c99
OfekCP <ofekzvulunov@gmail.com>
2024-03-07 15:17:03 +0200

    update

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const adminRoutes = require('./Routes/adminRoutes');
+const socialRoute=require('./Routes/socialRoutes')
 const userRoutes = require('./Routes/userRoutes');
 const app = express();
 
 app.use(express.json());
 app.use('/admin', adminRoutes);
-
+app.use('/social',socialRoute)
 module.exports = app;
