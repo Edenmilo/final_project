6fbe63a5ed475fa8e082d9919980d332630b8101
Eden <edenmilo31@gmail.com>
2024-03-07 12:24:04 +0200

    back

--- backend/Routes/adminRoutes.js
+++ backend/Routes/adminRoutes.js
@@ -1,21 +1,25 @@
 const express = require('express');
-const { createAdmin, createEvent, login,  createUser, logout, getUsersByAdminId, getAdminById, getEventsByAdminId, getEventRegisteredUsers, updateUser, deleteUser, getUserInfo, deleteEvent} = require('../Controllers/adminController');
+const { createAdmin, createEvent, login,  createUser, logout, getUsersByAdminId, getAdminById, getEventsByAdminId, getEventRegisteredUsers, updateUser, deleteUser, getUserInfo, deleteEvent, getEventById} = require('../Controllers/adminController');
 const router = express.Router();
 
-
 router.post('/login', login);
-router.post('/:adminId/createuser',  createUser);
 router.post('/logout', logout);
+
 router.post('/', createAdmin);
-router.post('/:adminId/create',createEvent)
-router.delete('/:adminId/:eventId/deleteevent', deleteEvent)
-router.get('/:adminId/getusers', getUsersByAdminId)
-router.get('/:adminId/:userId/getuser', getUserInfo)
 router.get('/:adminId/getinfo', getAdminById)
-router.get('/:adminId/getevents', getEventsByAdminId)
-router.get('/:adminId/:eventId/geteventusers', getEventRegisteredUsers)
+
+router.post('/:adminId/createuser',  createUser);
 router.put('/:adminId/:userId/updateuser', updateUser)
 router.delete('/:adminId/deleteuser', deleteUser)
+router.get('/:adminId/getusers', getUsersByAdminId)
+router.get('/:adminId/:userId/getuser', getUserInfo)
+
+router.post('/:adminId/create',createEvent)
+router.delete('/:adminId/:eventId/deleteevent', deleteEvent)
+router.get('/:adminId/getevents', getEventsByAdminId)
+router.get('/:adminId/:eventId/getevent', getEventById)
+
+router.get('/:adminId/:eventId/geteventusers', getEventRegisteredUsers)
 
 
 
