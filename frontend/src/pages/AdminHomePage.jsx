1f959ec56f23de1eb90b1824c168eff1e1c05f78
ארטיום אברמוב <artiom240648@gmail,com>
2024-03-11 04:00:02 +0200

    pull

--- frontend/src/pages/AdminHomePage.jsx
+++ frontend/src/pages/AdminHomePage.jsx
@@ -1,7 +1,8 @@
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
-import Calendar from "../components/calendar"
+import Calendar from "../components/Calendar";
+import CreateUser from "../components/CreateUser";
 // import UsersList from "./UsersList";
 
 function AdminHomePage() {
@@ -9,14 +10,15 @@ function AdminHomePage() {
   const { loginData, setLoginData } = useContext(AppContext);
   useEffect(() => {
     setLoginData(localStorage.getItem("loginData"));
-    // console.log(loginData);
+    console.log(loginData);
   });
 
   return (
     <>
       <div className="admin-home-container h-screen flex items-center justify-center">
-        <div className="admin-card w-[60vw] h-[90vh] overflow-y-auto bg-black-50 mt-[2vh]">
+        <div className="admin-card w-[60vw] h-[90vh] overflow-y-auto no-scrollbar bg-black-50 mt-[2vh]">
           <Calendar />
+          <CreateUser />
 
           {/* <div onClick={handelMyusersNavigation}>my students</div> */}
         </div>
--- frontend/src/pages/UsersList.jsx
+++ frontend/src/pages/UsersList.jsx
@@ -1,39 +1,53 @@
 import React from "react";
-import SmallUser from "../components/SmallUser";
+import AdminUsers from "../components/AdminUsers";
 import SearchBar from "../components/SearchBar";
-import CreatUser from "../components/CreateUser";
+// import CreateUser from "../components/CreateUser";
 
-function UsersList() {//gets admin id and fetches its users in useEffect()!!!
- 
-    let users = [
-        {
-          fullName: "omerbenezra",
-          phoneNumber: "0503220132",
-          email: "bene@gmail",
-        },
-        {
-          fullName: "edemilo",
-          phoneNumber: "0504705602",
-          email: "edenmilo@gmail",
-        },
-      ];
-  
+function UsersList() {
+  //gets admin id and fetches its users in useEffect()!!!
+
+  let users = [
+    {
+      fullName: "omerbenezra",
+      phoneNumber: "0503220132",
+      email: "bene@gmail",
+    },
+    {
+      fullName: "edemilo",
+      phoneNumber: "0504705602",
+      email: "edenmilo@gmail",
+    },
+  ];
 
   return (
     <>
-    <SearchBar users={users}/>
-    <ul>
-        {users.map((user, index) => (
-          <li key={index}>
-            <SmallUser user={user} />
-            <hr />
-          </li>
-            
-        ))}
-      </ul>
-      <CreatUser users={users}/>
+      <div className="users-list-container w-screen h-screen flex flex-col items-center justify-center">
+        <div className="users-list-card w-[40vw] h-[90vh] bg-black-50 rounded-[2rem]">
+          <div className="users-list-box w-full h-full">
+            <header className="users-list-header h-[25%]">
+              <h2 className="users-list-main-header text-[2rem] p-[1vh] text-neon-50">
+                Your Students List{" "}
+              </h2>
+              <SearchBar users={users} />
+            </header>
+            <hr className="hr-costume text-neon-50" />
+            <div className="users-list-content h-[75%]">
+              <ul className="list-element-UserList p-[0.4rem]">
+                {users.map((user, index) => (
+                  <li className="list-item-element p-[5px]" key={index}>
+                    <AdminUsers user={user} />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
+}
+{
+  /* <CreateUser users={users} /> */
 }
 
 export default UsersList;
