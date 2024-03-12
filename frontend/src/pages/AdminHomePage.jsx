3febe882d9e6fe5d5bca177c8e72d2f88001a336
Eden <edenmilo31@gmail.com>
2024-03-12 03:11:05 +0200

    eden

--- frontend/src/pages/AdminHomePage.jsx
+++ frontend/src/pages/AdminHomePage.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
-import Calendar from "../components/Calendar";
+import Calendar from "../components/calendar";
 import CreateUser from "../components/CreateUser";
 
 function AdminHomePage() {
