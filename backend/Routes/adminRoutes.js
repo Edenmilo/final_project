88a9815ca5baa19920c103e64be5c779070b625a
omerbenezra <122448600+omerbenezra@users.noreply.github.com>
2024-03-20 12:01:48 +0200

    update omer

--- backend/Routes/adminRoutes.js
+++ backend/Routes/adminRoutes.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const { logout, getAdminById, createUser, verifyToken, login, getUsersForAdmin } = require('../Controllers/adminController');
+const express = require("express");
+const {
+  logout,
+  getAdminById,
+  createUser,
+  verifyToken,
+  login,
+  getUsersForAdmin,
+} = require("../Controllers/adminController");
 const router = express.Router();
 
-
-
-router.post('/adminusers', verifyToken,getUsersForAdmin);
-router.get('/:adminId', getAdminById)
-router.post('/login', login);
-router.post('/logout', logout);
-router.post('/createuser', verifyToken, createUser);
-
-
+router.post("/adminusers", verifyToken, getUsersForAdmin);
+router.get("/:adminId", getAdminById);
+router.post("/login", login);
+router.post("/logout", logout);
+router.post("/createuser", verifyToken, createUser);
 
 module.exports = router;
