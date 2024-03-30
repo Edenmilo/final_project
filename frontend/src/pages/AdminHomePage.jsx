f090cc333f8ec5d376bb03d2aaa0fbec8e24ec7f
ארטיום אברמוב <artiom240648@gmail,com>
2024-03-31 02:24:16 +0300

    pull backend

--- frontend/src/pages/AdminHomePage.jsx
+++ frontend/src/pages/AdminHomePage.jsx
@@ -9,15 +9,17 @@ function AdminHomePage() {
   const navigate = useNavigate();
   const { loginData, setLoginData } = useContext(AppContext);
   useEffect(() => {
-    setLoginData(localStorage.getItem("loginData"));
-    console.log(loginData);
+    console.log(loginData.id);
   });
-
+  const handleUsersListClick = () => {
+    navigate("/usersList"); 
+  };
   return (
     <>
       <div className="admin-home-container h-screen flex items-center justify-center">
         <div className="admin-card w-[60vw] h-[90vh] overflow-y-auto no-scrollbar bg-black-50 mt-[2vh] phone:w-full phone:h-full phone:mt-0">
           <Calendar />
+          <button onClick={handleUsersListClick}>users list page</button>
           <CreatePost />
         </div>
       </div>
