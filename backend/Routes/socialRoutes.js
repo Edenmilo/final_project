d91067b65e348fac66e4329054a8981855f7664a
ארטיום אברמוב <artiom240648@gmail,com>
2024-04-09 22:35:48 +0300

    updated virsion

--- backend/Routes/socialRoutes.js
+++ backend/Routes/socialRoutes.js
@@ -6,19 +6,10 @@ const socialController = require("../Controllers/socialControllers");
 const router = express.Router();
 
 // Define the route for creating a post
-<<<<<<< HEAD
-// router.post('/post', createPost);
-router.post('/create',socialController.createPost)
-router.get('/all-socials',socialController.readPosts)
-router.get('/:socialId',socialController.getPostById)
-router.put('/update/:socialId',socialController.updatePost)
-router.delete('/delete/:socialId',socialController.deletePost)
-=======
 router.post("/post", socialController.createPost);
 router.get("/all-socials", socialController.readPosts);
 router.get("/:socialId", socialController.getPostById);
 router.put("/update/:socialId", socialController.updatePost);
 router.delete("/delete/:socialId", socialController.deletePost);
->>>>>>> e3ad4af5efed39314a1d0f35145440090369528e
 // Export the router
 module.exports = router;
